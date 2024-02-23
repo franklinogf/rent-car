1bfce6f1e205a77fec59cd39f376428580d8710a
DarwinML2 <darwinmass.ml2@gmail.com>
2024-02-23 02:38:16 -0400

    used ui Card

--- src/components/PopularCarSection.tsx
+++ src/components/PopularCarSection.tsx
@@ -1,44 +1,59 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
-import { CiHeart } from 'react-icons/ci'
 import { FaPeopleGroup } from 'react-icons/fa6'
 import { GiSteeringWheel } from 'react-icons/gi'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import { HeartIcon, PeopleIcon, SteeringIcon } from '@/lib/Icons'
 
 export function CarCard() {
   return (
-    <article className='w-[304px] bg-[#c1d4df] px-2 py-5'>
-      <div className='flex justify-between font-bold text-xl'>
-        <h3>Koenigsegg</h3>
-        <CiHeart className='size-8' />
-      </div>
-      <span className='text-[#90A3BF]'>Sport</span>
-      <Image
-        className='m-auto py-16'
-        alt='PopularCar'
-        src='/cars/car3.png'
-        width={232}
-        height={72}
-      />
-      <div className='flex justify-evenly'>
-        <span className='flex justify-evenly items-center'>
-          <GiSteeringWheel className='mr-1' />
-          Manual
-        </span>
-        <span className='flex justify-evenly items-center '>
-          <FaPeopleGroup className='mr-1' />
-          People
-        </span>
-      </div>
-      <div className='grid grid-cols-2 m-3 items-center'>
+    <Card className='w-full'>
+      <CardHeader>
+        <div className='flex justify-between'>
+          <CardTitle>Koenigsegg</CardTitle>
+          <HeartIcon />
+        </div>
+        <CardDescription>Sport</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Image
+          className='m-auto py-5'
+          alt='PopularCar'
+          src='/cars/car3.png'
+          width={232}
+          height={72}
+        />
+        <div className='flex justify-evenly'>
+          <span className='flex justify-evenly items-center'>
+            <SteeringIcon />
+            Manual
+          </span>
+          <span className='flex justify-evenly items-center '>
+            <PeopleIcon />
+            People
+          </span>
+        </div>
+      </CardContent>
+      <CardFooter className='flex justify-between'>
         <span>$99.00/day</span>
         <Button>Rent Now</Button>
-      </div>
-    </article>
+      </CardFooter>
+    </Card>
   )
 }
 export default function PopularSection() {
   return (
-    <section className='ml-56 mb-56'>
+    <section className='mt-3 grid grid-cols-4 gap-x-5 px-10'>
+      <CarCard />
+      <CarCard />
+      <CarCard />
       <CarCard />
     </section>
   )
