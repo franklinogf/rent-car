919507859351fefe4aa8ccf53668b6310a0f46b3
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-02-23 11:51:02 -0500

    headers now have links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,32 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 import { FaHeart } from 'react-icons/fa'
 import { IoSettingsSharp } from 'react-icons/io5'
-import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import Logo from '@/components/Logo'
 
 export default function Header() {
   return (
     <header className='bg-white w-full flex justify-between items-center px-4 shadow py-4'>
-      <h1 className='text-primary font-bold'>Rent Car</h1>
-      <Input
-        className='w-56 rounded-full'
-        placeholder='Search something here...'
-      />
+      <nav>
+        <Button
+          variant='ghost'
+          asChild
+          className='text-primary font-bold'
+        >
+          <Link href='/'>
+            <Logo />
+          </Link>
+        </Button>
+      </nav>
+      <nav className='flex justify-evenly items-center gap-4'>
+        <Button
+          variant='ghost'
+          asChild
+        >
+          <Link href='/cars'>All Cars</Link>
+        </Button>
+      </nav>
       <nav className='flex justify-evenly items-center gap-4'>
         <Button
           variant='ghost'
