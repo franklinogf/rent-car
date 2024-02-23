e5315629bf0a7d5f30a851dd40c5ea7ca0a73c5f
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-02-22 21:36:27 -0500

    moved header to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Header from '@/components/Header'
 import PreviewCar from '@/components/PreviewCar'
 
 export default function Page() {
   return (
     <>
-      <Header />
       <PreviewCar />
     </>
   )
