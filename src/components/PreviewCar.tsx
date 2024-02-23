4ccaff6c4ea44604ff90c1a3ce74c3898b4c90bb
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-02-22 22:09:41 -0500

    Updated the previewCar section

--- src/components/PreviewCar.tsx
+++ src/components/PreviewCar.tsx
@@ -1,34 +1,38 @@
 import { Button } from '@/components/ui/Button'
+import clsx from 'clsx'
 import Image from 'next/image'
 
-export default function PreviewCar() {
+export function Car() {
   return (
-    <>
-      <section className='bg-[#54A6FF] w-[640px] h-[360px] text-white rounded'>
-        <div className='grid grid-cols w-80 p-3 gap-5'>
-          <h2 className='font-bold text-2xl'>
-            The Best Platform for Car Rental
-          </h2>
-          <p className='font-light text-base'>
-            Ease of doing a carretal safety and rellably. Of course at low
-            price.
-          </p>
-          <Button
-            variant='outline'
-            className='rounded text-center w-32 py-5 text-base'
-          >
-            Rental Car
-          </Button>
-        </div>
-        <div className='flex justify-center'>
-          <Image
-            alt='previewCar1'
-            src='/cars/car1.png'
-            width={320}
-            height={116}
-          />
-        </div>
-      </section>
-    </>
+    <article className='bg-[#54A6FF] w-[640px] h-[360px] text-white relative mx-auto'>
+      <div className='grid grid-cols w-80 p-3 gap-5'>
+        <h2 className='font-bold text-2xl'>The Best Platform for Car Rental</h2>
+        <p className='font-light text-base'>
+          Ease of doing a carretal safety and rellably. Of course at low price.
+        </p>
+        <Button className='w-32 text-base'>Rental Car</Button>
+      </div>
+      <Image
+        className='bottom-0 left-36 absolute'
+        alt='previewCar1'
+        src='/cars/car2.png'
+        width={320}
+        height={116}
+      />
+    </article>
+  )
+}
+
+export default function PreviewCar({ className }: { className?: string }) {
+  return (
+    <section
+      className={clsx(
+        'w-full flex flex-col justify-center gap-y-5 xl:flex-row lg:justify-evenly',
+        className
+      )}
+    >
+      <Car />
+      <Car />
+    </section>
   )
 }
