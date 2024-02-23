e17ccd4deb363353cb381bd3fdc525d2399db918
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-02-22 22:09:06 -0500

    background color changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
+import clsx from 'clsx'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body className={clsx(inter.className, 'bg-slate-100/60')}>
         <Header />
         <main>{children}</main>
       </body>
