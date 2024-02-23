f22d945944904ba93aa27d5efcfb9548ff501a66
DarwinML2 <darwinmass.ml2@gmail.com>
2024-02-23 11:26:39 -0400

    created RecomendationSection

---
+++ src/components/ReconmendationSection.tsx
@@ -0,0 +1,21 @@
+import { CarCard } from './PopularCarSection'
+import { Button } from './ui/button'
+
+export default function RecomendationSection() {
+  return (
+    <section className='mt-4 items-center'>
+      <h2 className='px-10 font-semibold'>Recomendation Car</h2>
+      <div className='mt-3 grid grid-cols-4 gap-x-5 px-10'>
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+        <CarCard />
+      </div>
+      <Button>Show more Car</Button>
+    </section>
+  )
+}
