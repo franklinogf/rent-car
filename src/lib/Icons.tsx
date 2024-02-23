c3e05d0d16edc9f98787661fd4d9f3c1cf63da5f
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-02-23 11:38:38 -0500

    attributes to jsx attributes

--- src/lib/Icons.tsx
+++ src/lib/Icons.tsx
@@ -3,13 +3,13 @@ export function HeartIcon() {
     <svg
       fill='none'
       viewBox='0 0 24 24'
-      stroke-width='1.5'
+      strokeWidth='1.5'
       stroke='currentColor'
       className='w-6 h-6 hover:fill-red-500 hover:stroke-none hover:cursor-pointer'
     >
       <path
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeLinecap='round'
+        strokeLinejoin='round'
         d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z'
       />
     </svg>
@@ -21,14 +21,14 @@ export function PeopleIcon() {
       className='w-6 h-6 text-gray-800 dark:text-white'
       aria-hidden='true'
       fill='none'
-      stroke-width='1.5'
+      strokeWidth='1.5'
       stroke='currentColor'
       viewBox='0 0 24 24'
     >
       <path
-        fill-rule='evenodd'
+        fillRule='evenodd'
         d='M12 6a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Zm-1.5 8a4 4 0 0 0-4 4c0 1.1.9 2 2 2h7a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-3Zm6.8-3.1a5.5 5.5 0 0 0-2.8-6.3c.6-.4 1.3-.6 2-.6a3.5 3.5 0 0 1 .8 6.9Zm2.2 7.1h.5a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-1.1l-.5.8c1.9 1 3.1 3 3.1 5.2ZM4 7.5a3.5 3.5 0 0 1 5.5-2.9A5.5 5.5 0 0 0 6.7 11 3.5 3.5 0 0 1 4 7.5ZM7.1 12H6a4 4 0 0 0-4 4c0 1.1.9 2 2 2h.5a6 6 0 0 1 3-5.2l-.4-.8Z'
-        clip-rule='evenodd'
+        clipRule='evenodd'
       />
     </svg>
   )
