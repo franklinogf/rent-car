6f23aea4cf814750c449a535d455cc1c9f99e8c0
DarwinML2 <darwinmass.ml2@gmail.com>
2024-02-25 00:32:02 -0400

    added cars and specs

--- src/database/cars.ts
+++ src/database/cars.ts
@@ -36,5 +36,185 @@ export const cars: Car[] = [
         value: 5
       }
     ]
+  },
+  {
+    id:3,
+    brand: 'Rolls-Royce',
+    model: 'Dawn',
+    type: 'Sport',
+    image: '/cars/car3.png',
+    price: 30,
+    features: [
+      {
+        type: 'Gear',
+        value: 'Automatic'
+      },
+      {
+        type: 'Person',
+        value: 5
+      }
+    ]
+  },
+  {
+    id:4,
+    brand: 'Volkswagen',
+    model: 'Touareg',
+    type: 'SUV',
+    image: '/cars/car4.png',
+    price: 30,
+    features: [
+      {
+        type: 'Gear',
+        value: 'Automatic'
+      },
+      {
+        type: 'Person',
+        value: 5
+      }
+    ]
+  },
+  {
+    id:5,
+    brand: 'Nissan',
+    model: 'GT-R',
+    type: 'Sport',
+    image: '/cars/car5.png',
+    price: 30,
+    features: [
+      {
+        type: 'Gear',
+        value: 'Manual'
+      },
+      {
+        type: 'Person',
+        value: 4
+      }
+    ]
+  },
+  {
+    id:6,
+    brand: 'Nissan',
+    model: 'Skyline',
+    type: 'Sport',
+    image: '/cars/car6.png',
+    price: 30,
+    features: [
+      {
+        type: 'Gear',
+        value: 'Manual'
+      },
+      {
+        type: 'Person',
+        value: 4
+      }
+    ]
+  },
+  {
+    id:7,
+    brand: 'MG',
+    model: 'ZS',
+    type: 'SUV',
+    image: '/cars/car7.png',
+    price: 30,
+    features: [
+      {
+        type: 'Gear',
+        value: 'Automatic'
+      },
+      {
+        type: 'Person',
+        value: 5
+      }
+    ]
+  },
+  {
+    id:8,
+    brand: 'Chery',
+    model: 'Tiggo',
+    type: 'SUV',
+    image: '/cars/car8.png',
+    price: 30,
+    features: [
+      {
+        type: 'Gear',
+        value: 'Automatic'
+      },
+      {
+        type: 'Person',
+        value: 5
+      }
+    ]
+  },
+  {
+    id:9,
+    brand: 'MG',
+    model: 'ZX Exclusive',
+    type: 'Hatchback',
+    image: '/cars/car9.png',
+    price: 30,
+    features: [
+      {
+        type: 'Gear',
+        value: 'Automatic'
+      },
+      {
+        type: 'Person',
+        value: 5
+      }
+    ]
+  },
+  {
+    id:10,
+    brand: 'Porsche',
+    model: 'Cayenne',
+    type: 'SUV',
+    image: '/cars/car10.png',
+    price: 30,
+    features: [
+      {
+        type: 'Gear',
+        value: 'Automatic'
+      },
+      {
+        type: 'Person',
+        value: 7
+      }
+    ]
+  },
+  {
+    id:11,
+    brand: 'MG',
+    model: 'ZS',
+    type: 'SUV',
+    image: '/cars/car11.png',
+    price: 30,
+    features: [
+      {
+        type: 'Gear',
+        value: 'Automatic'
+      },
+      {
+        type: 'Person',
+        value: 5
+      }
+    ]
+  },
+  {
+    id:12,
+    brand: 'MG',
+    model: 'ZX Excite',
+    type: 'SUV',
+    image: '/cars/car12.png',
+    price: 30,
+    features: [
+      {
+        type: 'Gear',
+        value: 'Automatic'
+      },
+      {
+        type: 'Person',
+        value: 5
+      }
+    ]
   }
 ]
