aa932bc81549accccfbe42733987eb015d8cb96c
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-02-25 02:27:42 -0500

    aded bg to the preview cars

--- src/components/PreviewCar.tsx
+++ src/components/PreviewCar.tsx
@@ -1,15 +1,30 @@
-import { Button } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
 import Image from 'next/image'
+import { type VariantProps } from 'class-variance-authority'
 
-export function Car() {
+export function Car({
+  imageUrl,
+  buttonVariant
+}: {
+  imageUrl?: string
+  buttonVariant?: VariantProps<typeof buttonVariants>['variant']
+}) {
   return (
-    <article className='bg-blue-400 text-white relative p-4 w-full max-w-md h-[365px]'>
+    <article
+      className='bg-blue-400 text-white relative p-4 w-full max-w-md h-[365px] bg-cover bg-no-repeat rounded-[10px]'
+      style={{ backgroundImage: `url('${imageUrl}')` }}
+    >
       <div className='grid grid-cols w-80 gap-5'>
         <h2 className='font-bold text-2xl max-w-60'>The Best Platform for Car Rental</h2>
         <p className='font-light text-sm max-w-60'>
           Ease of doing a carretal safety and rellably. Of course at low price.
         </p>
-        <Button className='w-32 text-base'>Rental Car</Button>
+        <Button
+          variant={buttonVariant}
+          className='w-32 text-base'
+        >
+          Rental Car
+        </Button>
       </div>
       <Image
         className='bottom-0 left-1/2 -translate-x-1/2 lg:-translate-x-1/3 absolute w-48 lg:w-56'
@@ -26,8 +41,11 @@ export function Car() {
 export function PreviewCarSection() {
   return (
     <section className='w-full flex flex-col justify-center gap-y-5 lg:flex-row lg:justify-center lg:gap-x-5 mx-auto place-items-center'>
-      <Car />
-      <Car />
+      <Car imageUrl='/images/bg1.png' />
+      <Car
+        imageUrl='/images/bg2.png'
+        buttonVariant='secondary'
+      />
     </section>
   )
 }
