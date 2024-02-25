fa2b098cb49920f33120c1beeb79716297aad098
DarwinML2 <darwinmass.ml2@gmail.com>
2024-02-25 01:15:35 -0400

    added cars map method

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -2,10 +2,11 @@ import { CarCard } from '@/components/CarCard'
 import { Button } from '@/components/ui/button'
 import { Sidebar } from '@/components/Sidebar'
 import { LocationPicker } from '@/components/LocationPicker'
+import { cars } from '@/database/cars'
 
 export default function Page() {
   return (
-    <div className='grid grid-flow-col grid-cols-[20%_80%] h-dvh'>
+    <div className='grid grid-flow-col grid-cols-[20%_80%] h-full'>
       {/* <> */}
       <Sidebar />
       <div className='p-2'>
@@ -13,11 +14,12 @@ export default function Page() {
           <LocationPicker />
         </div>
         <section className='grid grid-cols-3 gap-8'>
-          {/* {Array(9)
-            .fill(null)
-            .map((car, index) => {
-              return <CarCard key={index} />
-            })} */}
+          {cars.map((car) => (
+            <CarCard
+              key={car.id}
+              {...car}
+            />
+          ))}
         </section>
         <Button className='mx-auto block mt-4'>Show more Car</Button>
       </div>
