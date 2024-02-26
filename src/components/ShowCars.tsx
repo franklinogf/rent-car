f8073b2de34780337cdfae019a31611c268a6228
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-02-25 22:17:29 -0500

    better responsive

--- src/components/ShowCars.tsx
+++ src/components/ShowCars.tsx
@@ -2,7 +2,7 @@ import { CarCard } from '@/components/CarCard'
 import { type Car } from '@/types/cars'
 export function ShowCars({ cars }: { cars: Car[] }) {
   return (
-    <div className='grid md:grid-cols-2 md:gap-x-5 lg:grid-cols-3 xl:grid-cols-4'>
+    <div className='grid md:grid-cols-2 md:gap-x-5 xl:grid-cols-4'>
       {cars.map((car) => (
         <CarCard
           key={car.id}
