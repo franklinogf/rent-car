5cae42ba8c8ae712a64be33f175d28f8b34e615a
DarwinML2 <darwinmass.ml2@gmail.com>
2024-02-27 00:07:41 -0400

    used BoxComment

--- src/app/cars/[id]/page.tsx
+++ src/app/cars/[id]/page.tsx
@@ -3,6 +3,7 @@ import { Alert, AlertDescription } from '@/components/ui/alert'
 
 import { CarGallery } from '@/components/CarGallery'
 import { CarDetail } from '@/components/CarDetail'
+import BoxComment from '@/components/BoxComments'
 
 export default function Page({ params }: { params: { id: string } }) {
   const car = cars.find((car) => car.id === Number(params.id))
@@ -19,6 +20,7 @@ export default function Page({ params }: { params: { id: string } }) {
     <div className='grid xl:grid-flow-col justify-center gap-5 mx-auto'>
       <CarGallery />
       <CarDetail car={car} />
+      <BoxComment />
     </div>
   )
 }
