1eee9dd4babb93b7a8a7ccd133a2cfd376db8bb4
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-05-20 13:13:58 -0400

    payload and next build test deployment

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { getPayloadClient } from "./payload/index.ts";
-import { nextApp, nextHandler } from "./lib/next-utils.ts";
+import { getPayloadClient } from "./payload/index";
+import { nextApp, nextHandler } from "./lib/next-utils";
 import path from "path";
 import dotenv from "dotenv";
+import nextBuild from "next/dist/build";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -20,16 +21,29 @@ const start = async () => {
     },
   });
 
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info("Next.js is building for production");
+
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, "../"));
+
+      process.exit();
+    });
+
+    return;
+  }
+
   app.use((req, res) => nextHandler(req, res));
 
   nextApp.prepare().then(() => {
     payload.logger.info("Next.js started");
-  });
 
-  app.listen(PORT, async () => {
-    payload.logger.info(
-      `Next.js app url: ${process.env.NEXT_PUBLIC_SERVER_URL}`
-    );
+    app.listen(PORT, async () => {
+      payload.logger.info(
+        `Next.js app url: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      );
+    });
   });
 };
 
