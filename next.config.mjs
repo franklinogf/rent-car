daaa469e7936c10f2c74c8b21b4ae573bdeb1433
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-05-20 18:55:04 -0400

    added images remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains:[
-            'http://localhost:3000'
+        remotePatterns:[
+            {
+                protocol: 'http',
+                hostname: 'localhost:3000',
+                pathname: '**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'rent-car-staging.up.railway.app',
+                pathname: '**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'rent-car-production-c392.up.railway.app',
+                pathname: '**'
+            }
         ]
     }
 };
